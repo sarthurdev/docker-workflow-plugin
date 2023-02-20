f203bd4e7ad4f463a9625adc71fd498a305a1edd
sarthurdev <965089+sarthurdev@users.noreply.github.com>
2023-02-20 20:06:37 +0100

    Sleep before executing docker top to allow Docker entrypoint execution

--- src/main/java/org/jenkinsci/plugins/docker/workflow/WithContainerStep.java
+++ src/main/java/org/jenkinsci/plugins/docker/workflow/WithContainerStep.java
@@ -196,6 +196,7 @@ public class WithContainerStep extends AbstractStepImpl {
 
             String command = launcher.isUnix() ? "cat" : "cmd.exe";
             container = dockerClient.run(env, step.image, step.args, ws, volumes, volumesFromContainers, envReduced, dockerClient.whoAmI(), /* expected to hang until killed */ command);
+            TimeUnit.SECONDS.sleep(2); // Hack to prevent race condition [JENKINS-54389]
             final List<String> ps = dockerClient.listProcess(env, container);
             if (!ps.contains(command)) {
                 listener.error(
